7de4a939f4cdf6ca68c7347489d71df1050da647
navhothi-cloud <65900207+navhothi-cloud@users.noreply.github.com>
2020-06-27 00:38:17 +0530

    Add files via upload

---
+++ Project 26/bob.js
@@ -0,0 +1,23 @@
+ class Bob {
+    constructor(x, y, radius){
+        var options={
+            isStatic:true,
+            resitution:0.3,
+            friction:0.5,
+            density:1.2
+          }
+        
+        this.body= Bodies.circle(x, y, radius, options);
+        this.radius=radius; 
+        World.add(world, this.body);
+        }
+    
+display(){
+  var pos =this.body.position;
+    push();
+    ellipseMode(CENTER);
+    ellipse(pos.x, pos.y, this.radius, this.radius);
+    pop();
+  }
+}
+ 
